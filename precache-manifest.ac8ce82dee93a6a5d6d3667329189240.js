fe57a363540c10b8fe50dacee54c4ba73bf136b1
SalmanKarim42 <salmankarimkhancs@outlook.com>
2020-08-08 23:08:25 +0500

    Updates

---
+++ precache-manifest.ac8ce82dee93a6a5d6d3667329189240.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dace431b400c791605f6a0e2b99c3367",
+    "url": "/Dev-camper/index.html"
+  },
+  {
+    "revision": "5ef7cd7de37b24d81f67",
+    "url": "/Dev-camper/static/css/2.daadad96.chunk.css"
+  },
+  {
+    "revision": "c0b45139a36b48e3f914",
+    "url": "/Dev-camper/static/css/main.d24cc473.chunk.css"
+  },
+  {
+    "revision": "5ef7cd7de37b24d81f67",
+    "url": "/Dev-camper/static/js/2.7a817f64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Dev-camper/static/js/2.7a817f64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b45139a36b48e3f914",
+    "url": "/Dev-camper/static/js/main.17d67f7d.chunk.js"
+  },
+  {
+    "revision": "8f8008361f3deca7181c",
+    "url": "/Dev-camper/static/js/runtime-main.704d472d.js"
+  },
+  {
+    "revision": "e58f20cbe9a6b8fe920f698efdffe05b",
+    "url": "/Dev-camper/static/media/image_1.e58f20cb.jpg"
+  },
+  {
+    "revision": "9feffbfa3c4412588b53725babe8068c",
+    "url": "/Dev-camper/static/media/image_2.9feffbfa.jpg"
+  },
+  {
+    "revision": "5daf784d1fce1ee059318df036ef395a",
+    "url": "/Dev-camper/static/media/image_3.5daf784d.jpg"
+  },
+  {
+    "revision": "13409133ba661f0c3ebebc134ec2f94a",
+    "url": "/Dev-camper/static/media/image_4.13409133.jpg"
+  },
+  {
+    "revision": "3fb08057b904438681073a33382606b0",
+    "url": "/Dev-camper/static/media/showcase.3fb08057.jpg"
+  }
+]);
